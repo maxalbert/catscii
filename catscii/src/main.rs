bb713561f11ffe94385da273ae9eedc5e90a2876
Maximilian Albert <maximilian.albert@gmail.com>
2024-06-09 14:22:39 +0000

    Retrieve cat picture URL from API

--- catscii/src/main.rs
+++ catscii/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
+        .await
+        .unwrap();
+    println!("Status: {}", res.status());
+    let body = res.text().await.unwrap();
+    println!("Body: {}", body);
 }
