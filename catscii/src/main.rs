e0e5147e342d20e4eacfe34aec88888638585a5f
Maximilian Albert <maximilian.albert@gmail.com>
2024-06-10 19:08:08 +0000

    Improved error handling

--- catscii/src/main.rs
+++ catscii/src/main.rs
@@ -3,21 +3,29 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
-        .await
-        .unwrap();
-    if !res.status().is_success() {
-        panic!("Request failed with HTTP {}", res.status());
-    }
-
-#[derive(Deserialize)]
-struct CatImage {
-    url: String,
+    let url = get_cat_image_url().await.unwrap();
+    println!("The image is at {}", url);
 }
 
-    let images: Vec<CatImage> = res.json().await.unwrap();
-    let image = images
-        .first()
-        .expect("the cat API should return at least one image");
-    println!("The image is at {}", image.url);
+async fn get_cat_image_url() -> color_eyre::Result<String> {
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let res = reqwest::get(api_url).await?;
+    if !res.status().is_success() {
+        return Err(color_eyre::eyre::eyre!(
+            "The Cat API returned HTTP {}",
+            res.status()
+        ));
+    }
+
+    #[derive(Deserialize)]
+    struct CatImage {
+        url: String,
+    }
+    let images: Vec<CatImage> = res.json().await?;
+    // this syntax is new in Rust 1.65
+    let Some(image) = images.into_iter().next() else {
+        return Err(color_eyre::eyre::eyre!("The Cat API returned no images"));
+    };
+
+    Ok(image.url)
 }
